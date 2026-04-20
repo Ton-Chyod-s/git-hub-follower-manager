26d769c1f72ab63088dc60ff86664bc4a905f2f2
Ton-Chyod-s <klayton.dias@hotmail.com>
2026-04-20 19:58:39 -0400

    fix: ajustar configuração do servidor para usar a porta do ambiente e adicionar script heroku-postbuild ao package.json

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express, { Application } from 'express';
 import { RegisterRoutes } from './src/controllers/RegisterRoutes';
 
-const server: Application  = express();
-const port = 3000;
+const server: Application = express();
+const port = process.env.PORT || 3000;
 
 server.use(express.json());
 
