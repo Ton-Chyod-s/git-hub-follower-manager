361cddeb773ef25923cfb3ca20219e111b534648
Ton-Chyod-s <klayton.dias@hotmail.com>
2026-04-23 16:35:51 -0400

    feat: atualizar configuração do Prisma, removendo arquivos obsoletos e adicionando novos arquivos de migração

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,26 @@
-import path from 'node:path';
-import { defineConfig } from 'prisma/config';
+import dotenv from 'dotenv';
+import path from 'path';
+import fs from 'fs';
+import { defineConfig, env as prismaEnv } from 'prisma/config';
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+
+const envFiles = [`.env.${nodeEnv}`, '.env'].filter(Boolean) as string[];
+
+for (const envFile of envFiles) {
+  const envPath = path.resolve(process.cwd(), envFile);
+  if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath });
+    break;
+  }
+}
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
+  schema: 'prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: prismaEnv('DATABASE_URL'),
+  },
 });
---
+++ prisma/migrations/20260423203528_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateEnum
+CREATE TYPE "user_role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "refresh_tokens" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "user_id" UUID NOT NULL,
+    "token_hash" TEXT NOT NULL,
+    "expires_at" TIMESTAMPTZ(6) NOT NULL,
+    "used_at" TIMESTAMPTZ(6),
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "refresh_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "name" VARCHAR(100) NOT NULL,
+    "email" CITEXT NOT NULL,
+    "password_hash" TEXT,
+    "github_id" TEXT,
+    "role" "user_role" NOT NULL DEFAULT 'USER',
+    "token_version" INTEGER NOT NULL DEFAULT 0,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_token_hash_key" ON "refresh_tokens"("token_hash");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_github_id_key" ON "users"("github_id");
+
+-- AddForeignKey
+ALTER TABLE "refresh_tokens" ADD CONSTRAINT "refresh_tokens_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
